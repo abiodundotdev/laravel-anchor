f501d37184e08788e6bd4396784e5f649b6d97bf
Qazeem Abiodun <qudusqazeem@gmail.com>
2023-09-10 07:13:31 +0100

    Updated Onboarding class

--- src/Sdk/AnchorOnboarding.php
+++ src/Sdk/AnchorOnboarding.php
@@ -2,28 +2,96 @@
 namespace Anchor\Sdk;
 use Anchor\Constants\Endpoints;
 class AnchorOnboarding extends AnchorHttp{
-
-    public $endpoint = Endpoints::$customers;
-
-    public function createCustomer(array $data){
-        return $this->post("");
+    private $customerEndpoint = Endpoints::$customers;
+    private $documentEndpoint = Endpoints::$documents;
+    private $individualEndpoint = Endpoints::$individual;
+    /** Create a customer
+    *
+    */
+    public function createCustomer(array $data, $type){
+        $body =  [
+            "data" => $data, 
+            "type" => $type 
+         ];
+        return $this->post($this->customerEndpoint, $body);
     }
-    public function getCustomer($customerId, $include){
-        return $this->get($this->endpoint+"/$customerId",  ['include' => $include]);
+    public function updateCustomer(array $data, $type){
+        $body =  [
+            "data" => $data, 
+            "type" => $type 
+         ];
+        return $this->post($this->customerEndpoint, $body);
     }
-    public function getAllCustomers($customerType= "", $metadata = "", $query = "", $page = "", $size = "", $sort = "", $include= ""){
-        return $this->get($this->endpoint,
+    public function fetchCustomer($customerId, $include = ""){
+        return $this->get($this->customerEndpoint."/$customerId",  ['include' => $include]);
+    }
+    public function fetchAllCustomers($customerType= "", $metadata = "", $query = "", $page = "", $size = "", $sort = "", $include= ""){
+        return $this->get($this->customerEndpoint,
           ['customerType'=> $customerType, 'metadata' => $metadata, 'query' => $query, ,'page'=> $page, 'size' => $size, 'sort' =>  $sort, 'include' => $include]
         );
     }
     public function deleteCustomer($customerId){
-        return $this->delete($this->endpoint+"/$customerId");
+        return $this->delete($this->customerEndpoint."/$customerId");
     }
-    public function searchCustomer($customerType, $searchValue,  $include){
-        return $this->get($this->endpoint+"/search",  ['customerType' => $customerType,  'searchValue' => $searchValue, 'include' => $include]);
+    public function searchCustomer($customerType, $searchValue,  $include = ""){
+        return $this->get($this->customerEndpoint."/search",  ['customerType' => $customerType,  'searchValue' => $searchValue, 'include' => $include]);
     }
     public function officersRequirement($customerId, $include = ""){
-        return $this->get($this->endpoint+"/$customerId/officers-requirement",  ['include' => $include]);
+        return $this->get($this->customerEndpoint."/$customerId/officers-requirement",  ['include' => $include]);
+    }
+
+    public function validateKYCForIndividual($customerId, array $data = []){
+        return $this->post($this->customerEndpoint."/$customerId/verification/individual",  ['data' => $data]);
+    }
+
+    public function validateKYCForBusiness($customerId){
+        return $this->post($this->customerEndpoint."/$customerId/verification/business");
+    }
+
+    public function downloadDocument($customerId, $documentId){
+        return $this->get($this->documentEndpoint."download-document/$customerId/$documentId");
+    }
+
+    public function fetchDocument($customerId, $documentId, $include = ""){
+        return $this->get($this->documentEndpoint."/$customerId/$documentId", ['include' => $include]);
+    }
+
+    public function fetchAllDocuments($customerId, $include = ""){
+        return $this->get($this->documentEndpoint."/$customerId", ['include' => $include]);
+    }
+
+    public function uploadDocument($customerId, $documentId, $textData = "", array $data){
+        $body =  [
+            "data" => $data, 
+        ];
+        return $this->post($this->documentEndpoint."/$customerId/$documentId?textData=$textData", $body);
+    }
+
+    public function createNextOfKin($customerId, array $data){
+        $body =  [
+            "data" => $data, 
+        ];
+        return $this->post($this->individualEndpoint."/$customerId/next-of-kin", $body);
+    }
+
+    public function updateNextOfKin($customerId, $nexOfKinId, array $data){
+        $body =  [
+            "data" => $data, 
+        ];
+        return $this->post($this->individualEndpoint."/$customerId/next-of-kin/$nexOfKinId", $body);
+    }
+    public function fetchNextOfKin($customerId, $nexOfKinId, $include = ""){
+        return $this->get($this->individualEndpoint."/$customerId/next-of-kin/$nexOfKinId", ['include' => $include]);
+
+    }
+
+    public function fetchAllNextOfKins($customerId, $include = ""){
+        return $this->get($this->individualEndpoint."/$customerId/next-of-kin", ['include' => $include]);
+
+    }
+
+    public function deleteNextOfKin($customerId, $nexOfKinId){
+        return $this->delete($this->individualEndpoint."/$customerId/next-of-kin/$nexOfKinId");
     }
 }
 
